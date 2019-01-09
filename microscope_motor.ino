8a9570cbfa34cece22672f217cfdd8ab5c25a794
Pierre Baudin <pvbaudin@ucsc.edu>
2019-01-09 15:43:11 -0800

    Update microscope_motor.ino
    added gfp light controller functionality

--- microscope_motor.ino
+++ microscope_motor.ino
@@ -16,11 +16,13 @@
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_MotorShield LightController = Adafruit_MotorShield(0x61);
 
 //L -> R
 //Yellow, Orange, NC, Brown, Redaaaaa
 Adafruit_StepperMotor * myMotor1 = AFMS.getStepper(200,  1);
 Adafruit_StepperMotor *myMotor2 = AFMS.getStepper(200,  2);
+Adafruit_DCMotor *light= LightController.getMotor(1);
 
 int step_count[2] = {0, 0};
 int brightness = 100;
@@ -35,61 +37,107 @@ void setup() {
   Serial.println("1 to increase brightness and 2 to decrease brightness");
 
   AFMS.begin();  // create with the default frequency 1.6KHz
+  LightController.begin();
+  
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   pinMode(3, OUTPUT);
   analogWrite(3, brightness);
   myMotor1->setSpeed(200);  // 10 rpm
   myMotor2->setSpeed(200);
   step_time = millis();
+
+  //for lights
+  light->setSpeed(100);
+  light->run(FORWARD);
+  // turn on motor
+  light->run(RELEASE);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-
+    uint8_t i;
     int inByte = Serial.read();
-    //Serial.println(inByte);
-    switch (inByte) {
-      case 'q':
-        step_count[0] = step_count[0] + 100;
-        step_count[1] = step_count[1] + 100;
-        break;
-      case 'w':
-        step_count[0] = step_count[0] - 100;
-        step_count[1] = step_count[1] - 100;
-        break;
-      case 'a':
-        step_count[0] = step_count[0] + 10;
-        step_count[1] = step_count[1] + 10;
-        break;
-      case 's':
-        step_count[0] = step_count[0] - 10;
-        step_count[1] = step_count[1] - 10;
-        break;
-      case 'z':
-        step_count[0] = step_count[0] + 1;
-        step_count[1] = step_count[1] + 1;
-        break;
-      case 'x':
-        step_count[0] = step_count[0] - 1;
-        step_count[1] = step_count[1] - 1;
-        break;
-      case '1':
-        brightness = (brightness + 10);
-        if (brightness > 255) {
-          brightness = 255;
-        }
-        analogWrite(3, brightness);
-        break;
-      case '2':
-        brightness = (brightness - 10) ;
-        if (brightness < 0) {
-          brightness = 0;
-        }
-        analogWrite(3, brightness);
-        break;
-      case '0':
-        step_count[0] = 0;
-        step_count[1] = 0;
+    if (inByte == '0')
+    {
+      light->setSpeed(0);
+      light->run(RELEASE);
+    }
+    else if (inByte == '1')
+    {
+      light->setSpeed(30);
+      light->run(FORWARD);
+    }
+    else if (inByte == '2')
+    {
+      light->setSpeed(80);
+      light->run(FORWARD);
+    }
+    else if (inByte == '3')
+    {
+      light->setSpeed(120);
+      light->run(FORWARD);
+    }
+    else if (inByte == '4')
+    {
+      light->setSpeed(170);
+      light->run(FORWARD);
+    }
+    else if (inByte == '5')
+    {
+      light->setSpeed(220);
+      light->run(FORWARD);
+    }
+    else if (inByte == '6')
+    {
+      light->setSpeed(255);
+      light->run(FORWARD);
+    }
+    else
+    {
+      //Serial.println(inByte);
+      switch (inByte) {
+        case 'q':
+          step_count[0] = step_count[0] + 100;
+          step_count[1] = step_count[1] + 100;
+          break;
+        case 'w':
+          step_count[0] = step_count[0] - 100;
+          step_count[1] = step_count[1] - 100;
+          break;
+        case 'a':
+          step_count[0] = step_count[0] + 10;
+          step_count[1] = step_count[1] + 10;
+          break;
+        case 's':
+          step_count[0] = step_count[0] - 10;
+          step_count[1] = step_count[1] - 10;
+          break;
+        case 'z':
+          step_count[0] = step_count[0] + 1;
+          step_count[1] = step_count[1] + 1;
+          break;
+        case 'x':
+          step_count[0] = step_count[0] - 1;
+          step_count[1] = step_count[1] - 1;
+          break;
+          //      case '1':
+          //        brightness = (brightness + 10);
+          //        if (brightness > 255) {
+          //          brightness = 255;
+          //        }
+          //        analogWrite(3, brightness);
+          //        break;
+          //      case '2':
+          //        brightness = (brightness - 10) ;
+          //        if (brightness < 0) {
+          //          brightness = 0;
+          //        }
+          //        analogWrite(3, brightness);
+          //        break;
+          //      case '0':
+          //        step_count[0] = 0;
+          //        step_count[1] = 0;
+      }
     }
   }
 
@@ -101,7 +149,7 @@ void loop() {
   {
     myMotor2->release();
   }
-  
+
   if (step_count[0] > 0)
   {
     myMotor1->step(1, FORWARD, SINGLE);
@@ -127,11 +175,11 @@ void loop() {
     if (millis() - step_time >= delay_time)
     {
     /*
-      myMotor->step(1, FORWARD, SINGLE);
+      light->step(1, FORWARD, SINGLE);
       if (delay_time >= 2000)
       {
       delay(1000);
-      myMotor->release();
+      light->release();
       }*/
   //  step_time = millis();
   //}

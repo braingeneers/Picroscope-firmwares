8342a50d2847deebb2254cb5c4b76cfbd25f4ca8
Pierre Baudin <pvbaudin@ucsc.edu>
2019-01-10 15:57:04 -0800

    Making height adjustment continuous

--- microscope_motor.ino
+++ microscope_motor.ino
@@ -22,12 +22,19 @@ Adafruit_MotorShield LightController = Adafruit_MotorShield(0x61);
 //Yellow, Orange, NC, Brown, Redaaaaa
 Adafruit_StepperMotor * myMotor1 = AFMS.getStepper(200,  1);
 Adafruit_StepperMotor *myMotor2 = AFMS.getStepper(200,  2);
-Adafruit_DCMotor *light= LightController.getMotor(1);
+Adafruit_DCMotor *light = LightController.getMotor(1);
 
+char inData[80];
+byte index = 0;
 int step_count[2] = {0, 0};
 int brightness = 100;
 long step_time;
 int delay_time = 20000;
+int curMotorPosition = 0;
+int newMotorPosition = 0;
+int stepsToTake = 0;
+
+
 void setup() {
   Serial.begin(115200);           // set up Serial library at 9600 bps
   Serial.println("Microscope Stepper controller");
@@ -38,7 +45,7 @@ void setup() {
 
   AFMS.begin();  // create with the default frequency 1.6KHz
   LightController.begin();
-  
+
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   pinMode(3, OUTPUT);
   analogWrite(3, brightness);
@@ -52,125 +59,159 @@ void setup() {
   // turn on motor
   light->run(RELEASE);
 }
-
+int val = -1;
+char a = 'n';
+char b = 'n';
 void loop() {
-  if (Serial.available() > 0) {
-    uint8_t i;
-    int inByte = Serial.read();
-    if (inByte == '0')
-    {
-      light->setSpeed(0);
-      light->run(RELEASE);
-    }
-    else if (inByte == '1')
-    {
-      light->setSpeed(30);
-      light->run(FORWARD);
-    }
-    else if (inByte == '2')
-    {
-      light->setSpeed(80);
-      light->run(FORWARD);
-    }
-    else if (inByte == '3')
-    {
-      light->setSpeed(120);
-      light->run(FORWARD);
-    }
-    else if (inByte == '4')
-    {
-      light->setSpeed(170);
-      light->run(FORWARD);
-    }
-    else if (inByte == '5')
-    {
-      light->setSpeed(220);
-      light->run(FORWARD);
-    }
-    else if (inByte == '6')
-    {
-      light->setSpeed(255);
-      light->run(FORWARD);
-    }
-    else
-    {
-      //Serial.println(inByte);
-      switch (inByte) {
-        case 'q':
-          step_count[0] = step_count[0] + 100;
-          step_count[1] = step_count[1] + 100;
-          break;
-        case 'w':
-          step_count[0] = step_count[0] - 100;
-          step_count[1] = step_count[1] - 100;
-          break;
-        case 'a':
-          step_count[0] = step_count[0] + 10;
-          step_count[1] = step_count[1] + 10;
-          break;
-        case 's':
-          step_count[0] = step_count[0] - 10;
-          step_count[1] = step_count[1] - 10;
-          break;
-        case 'z':
-          step_count[0] = step_count[0] + 1;
-          step_count[1] = step_count[1] + 1;
-          break;
-        case 'x':
-          step_count[0] = step_count[0] - 1;
-          step_count[1] = step_count[1] - 1;
-          break;
-          //      case '1':
-          //        brightness = (brightness + 10);
-          //        if (brightness > 255) {
-          //          brightness = 255;
-          //        }
-          //        analogWrite(3, brightness);
-          //        break;
-          //      case '2':
-          //        brightness = (brightness - 10) ;
-          //        if (brightness < 0) {
-          //          brightness = 0;
-          //        }
-          //        analogWrite(3, brightness);
-          //        break;
-          //      case '0':
-          //        step_count[0] = 0;
-          //        step_count[1] = 0;
-      }
-    }
+  if (Serial.available() >= 2) {
+     a = Serial.read();
+     b = Serial.read();
+     val = Serial.parseInt();
   }
-
-  if (step_count[0] == 0)
-  {
+  Serial.println(a);
+  Serial.println(b);
+  Serial.println(val, DEC);
+  
+  if ( a == 'm'){
+    if (val > -1000) && (val < 1000)
+      newMotorPosition = val;
+  }
+  stepsToTake = newMotorPosition - curMotorPosition;
+  if( stepsToTake > 0) {
+    myMotor1->step(1, FORWARD, SINGLE);
+    myMotor2->step(1, FORWARD, SINGLE);
+    curMotorPosition++;
+  }
+  else if ( stepsToTake < 0) {
+    myMotor1->step(1, BACKWARD, SINGLE);
+    myMotor2->step(1, BACKWARD, SINGLE);
+    curMotorPosition--;
+  }
+  else{
     myMotor1->release();
-  }
-  if (step_count[1] == 0)
-  {
     myMotor2->release();
   }
-
-  if (step_count[0] > 0)
-  {
-    myMotor1->step(1, FORWARD, SINGLE);
-    step_count[0]--;
-  }
-  else if (step_count[0] < 0)
-  {
-    myMotor1->step(1, BACKWARD, SINGLE);
-    step_count[0]++;
-  }
-
-  if (step_count[1] > 0)
-  {
-    myMotor2->step(1, FORWARD, SINGLE);
-    step_count[1]--;
-  }
-  else if (step_count[1] < 0)
-  {
-    myMotor2->step(1, BACKWARD, SINGLE);
-    step_count[1]++;
-  }
+  
+    
+//  
+//  if (Serial.available() > 0) {
+//    uint8_t i;
+//    //int inByte = Serial.read();
+//
+//    if (inByte == '0')
+//    {
+//      light->setSpeed(0);
+//      light->run(RELEASE);
+//    }
+//    else if (inByte == '1')
+//    {
+//      light->setSpeed(30);
+//      light->run(FORWARD);
+//    }
+//    else if (inByte == '2')
+//    {
+//      light->setSpeed(80);
+//      light->run(FORWARD);
+//    }
+//    else if (inByte == '3')
+//    {
+//      light->setSpeed(120);
+//      light->run(FORWARD);
+//    }
+//    else if (inByte == '4')
+//    {
+//      light->setSpeed(170);
+//      light->run(FORWARD);
+//    }
+//    else if (inByte == '5')
+//    {
+//      light->setSpeed(220);
+//      light->run(FORWARD);
+//    }
+//    else if (inByte == '6')
+//    {
+//      light->setSpeed(255);
+//      light->run(FORWARD);
+//    }
+//    else
+//    {
+//      //Serial.println(inByte);
+//      switch (inByte) {
+//        case 'q':
+//          step_count[0] = step_count[0] + 100;
+//          step_count[1] = step_count[1] + 100;
+//          break;
+//        case 'w':
+//          step_count[0] = step_count[0] - 100;
+//          step_count[1] = step_count[1] - 100;
+//          break;
+//        case 'a':
+//          step_count[0] = step_count[0] + 10;
+//          step_count[1] = step_count[1] + 10;
+//          break;
+//        case 's':
+//          step_count[0] = step_count[0] - 10;
+//          step_count[1] = step_count[1] - 10;
+//          break;
+//        case 'z':
+//          step_count[0] = step_count[0] + 1;
+//          step_count[1] = step_count[1] + 1;
+//          break;
+//        case 'x':
+//          step_count[0] = step_count[0] - 1;
+//          step_count[1] = step_count[1] - 1;
+//          break;
+//          //      case '1':
+//          //        brightness = (brightness + 10);
+//          //        if (brightness > 255) {
+//          //          brightness = 255;
+//          //        }
+//          //        analogWrite(3, brightness);
+//          //        break;
+//          //      case '2':
+//          //        brightness = (brightness - 10) ;
+//          //        if (brightness < 0) {
+//          //          brightness = 0;
+//          //        }
+//          //        analogWrite(3, brightness);
+//          //        break;
+//          //      case '0':
+//          //        step_count[0] = 0;
+//          //        step_count[1] = 0;
+//      }
+//    }
+//  }
+//
+//  if (step_count[0] == 0)
+//  {
+//    myMotor1->release();
+//  }
+//  if (step_count[1] == 0)
+//  {
+//    myMotor2->release();
+//  }
+//
+//  if (step_count[0] > 0)
+//  {
+//    myMotor1->step(1, FORWARD, SINGLE);
+//    step_count[0]--;
+//  }
+//  else if (step_count[0] < 0)
+//  {
+//    myMotor1->step(1, BACKWARD, SINGLE);
+//    step_count[0]++;
+//  }
+//
+//  if (step_count[1] > 0)
+//  {
+//    myMotor2->step(1, FORWARD, SINGLE);
+//    step_count[1]--;
+//  }
+//  else if (step_count[1] < 0)
+//  {
+//    myMotor2->step(1, BACKWARD, SINGLE);
+//    step_count[1]++;
+//  }
   /*
     if (millis() - step_time >= delay_time)
     {

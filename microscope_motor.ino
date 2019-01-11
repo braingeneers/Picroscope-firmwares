6dd7c4544dd6f3f193c971995de802bb78a92994
Pierre Baudin <pvbaudin@ucsc.edu>
2019-01-10 17:17:06 -0800

    Added EEPROM, Cleaned up comments, deleted old code

--- microscope_motor.ino
+++ microscope_motor.ino
@@ -1,18 +1,12 @@
-
-
 /*
-  This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-  It won't work with v1.x motor shields! Only for the v2's with built in PWM
-  control
+This code is for running the stepper motors and GFP light board for the Braingeneers PiCroscope Project
 
-  For use with the Adafruit Motor Shield v2
-  ---->  http://www.adafruit.com/products/1438
+
 */
-
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
+#include "EEPROM.h"
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
@@ -24,34 +18,24 @@ Adafruit_StepperMotor * myMotor1 = AFMS.getStepper(200,  1);
 Adafruit_StepperMotor *myMotor2 = AFMS.getStepper(200,  2);
 Adafruit_DCMotor *light = LightController.getMotor(1);
 
-char inData[80];
+
 byte index = 0;
+int address = 0;
 int step_count[2] = {0, 0};
-int brightness = 100;
-long step_time;
-int delay_time = 20000;
-int curMotorPosition = 0;
-int newMotorPosition = 0;
+int curMotorPosition = EEPROM.read(0);
+int newMotorPosition = EEPROM.read(0);
 int stepsToTake = 0;
 
 
 void setup() {
   Serial.begin(115200);           // set up Serial library at 9600 bps
-  //  Serial.println("Microscope Stepper controller");
-  //  Serial.println("q to move up 100 steps and w to move down 100 steps");
-  //  Serial.println("a to move up 10 steps and s to move down 10 steps");
-  //  Serial.println("z to move up 1 steps and x to move down 1 steps");
-  //  Serial.println("1 to increase brightness and 2 to decrease brightness");
 
   AFMS.begin();  // create with the default frequency 1.6KHz
   LightController.begin();
 
-  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
-  pinMode(3, OUTPUT);
-  analogWrite(3, brightness);
   myMotor1->setSpeed(200);  // 10 rpm
   myMotor2->setSpeed(200);
-  step_time = millis();
+
 
   //for lights
   light->setSpeed(100);
@@ -62,19 +46,19 @@ void setup() {
 int val = -1;
 char a = 'n';
 char b = 'n';
+
 void loop() {
   if (Serial.available() >= 2) {
     a = Serial.read();
     b = Serial.read();
     val = Serial.parseInt();
   }
-  //  Serial.println(a);
-  //  Serial.println(b);
-  //  Serial.println(val, DEC);
-
+  
   if ( a == 'm') {
     if ((val > -1000) && (val < 1000))
       newMotorPosition = val;
+      //save the new motor position into EEPROM
+      EEPROM.update(address, val);
   }
   stepsToTake = newMotorPosition - curMotorPosition;
   if ( stepsToTake > 0) {
@@ -91,6 +75,8 @@ void loop() {
     myMotor1->release();
     myMotor2->release();
   }
+
+  
   if (a == 'l') {
     if (val > 0 && val <= 255) {
       light->setSpeed(val);

3a4b9b6bfefbd018b6923288d5edc8ae11fc84c1
Pierre Baudin <pvbaudin@ucsc.edu>
2020-11-05 18:02:00 -0800

    updated script

--- Linux-layer/Testing-Scripts/turn-on-node-red.sh
+++ Linux-layer/Testing-Scripts/turn-on-node-red.sh
@@ -1,1 +1,1 @@
-for i in {1..3}; do for j in {2..5}; do ssh -t pi@cameraC$i$j.local "sudo systemctl enable nodered.service && sudo reboot"; done; done
+for i in {1..4}; do for j in {1..6}; do ssh -t pi@cameraC$i$j.local "sudo systemctl enable nodered.service && sudo reboot"; done; done

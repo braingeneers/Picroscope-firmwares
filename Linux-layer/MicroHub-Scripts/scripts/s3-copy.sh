8ed9070604757941d0234adeed7830a1dbd08194
Pierre Baudin <pvbaudin@ucsc.edu>
2022-01-19 15:08:18 -0800

    updated s3-copy script to reflect standard prp profile naming convention

--- Linux-layer/MicroHub-Scripts/scripts/s3-copy.sh
+++ Linux-layer/MicroHub-Scripts/scripts/s3-copy.sh
@@ -1,2 +1,2 @@
-aws --profile prp --endpoint https://s3.nautilus.optiputer.net s3 cp /home/pi/Pictures/$1 s3://braingeneers/imaging/$1/images --acl public-read --recursive 
+aws --profile prp-braingeneers --endpoint https://s3.nautilus.optiputer.net s3 cp /home/pi/Pictures/$1 s3://braingeneers/imaging/$1/images --acl public-read --recursive 
 

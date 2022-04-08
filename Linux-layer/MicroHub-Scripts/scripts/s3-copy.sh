6b0bfc6bab8cc57cb93c5b650851ed2b7c3eaebe
Pierre Baudin <pvbaudin@ucsc.edu>
2022-04-08 16:56:00 -0700

    cleaned up microhub scripts folder and updated endpoint

--- Linux-layer/MicroHub-Scripts/scripts/s3-copy.sh
+++ Linux-layer/MicroHub-Scripts/scripts/s3-copy.sh
@@ -1,2 +1,1 @@
-aws --profile prp-braingeneers --endpoint https://s3.nautilus.optiputer.net s3 cp /home/pi/Pictures/$1 s3://braingeneers/imaging/$1/images --acl public-read --recursive 
-
+aws --profile prp-braingeneers --endpoint https://s3-west.nrp-nautilus.io s3 cp /home/pi/Pictures/$1 s3://braingeneers/imaging/$1/images --acl public-read --recursive 
--- Linux-layer/MicroHub-Scripts/scripts/sendNewest.sh
+++
@@ -1,1 +0,0 @@
-~/scripts/copySend.sh /var/www/html/media/$(ls /var/www/html/media/ -t | head -1) $(hostname)

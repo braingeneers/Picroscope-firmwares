456359ae8101d71884800299e90f59722d89f7e8
Pierre Baudin <pvbaudin@ucsc.edu>
2022-11-10 14:29:10 -0800

    changed how add-network.sh works

--- Linux-layer/CameraEnd-Scripts/scripts/add-network.sh
+++ Linux-layer/CameraEnd-Scripts/scripts/add-network.sh
@@ -1,22 +1,13 @@
 #!/bin/sh
 
-SSID="$(echo $1 | cut -d ':' -f 2)"
-PASSWORD="$(echo $1 | cut -d ':' -f 3)"
-PRIORITY="$(grep priority= < /etc/wpa_supplicant/wpa_supplicant.conf | grep -o '.$' | tail -1)"
-
-if [ -z "$PRIORITY" ]
-then
-	PRIORITY=1
-else
-	PRIORITY=$((PRIORITY+1))
-fi
-
 cat >> /etc/wpa_supplicant/wpa_supplicant.conf << EOM
 
 network={
-    ssid="$SSID"
-    psk="$PASSWORD"
-    priority=$PRIORITY
+    ssid="$1"
+    scan_ssid=1
+    key_mgmt=WPA-PSK
+    psk="$2"
+    priority=$3
 }
 EOM
 

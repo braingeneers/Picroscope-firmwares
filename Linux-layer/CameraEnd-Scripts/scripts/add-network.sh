0f803f589471673d4157dc8084b34e20a3431343
Pierre Baudin <pvbaudin@ucsc.edu>
2022-11-10 15:02:14 -0800

    changed add-network to avoid adding same network multiple times

--- Linux-layer/CameraEnd-Scripts/scripts/add-network.sh
+++ Linux-layer/CameraEnd-Scripts/scripts/add-network.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+SSID="ssid=\""$1"\""
+
+#if grep -Fq "$SSID" /Users/pierre/Braingeneers/Picroscope-Firmwares/Linux-layer/CameraEnd-Scripts/scripts/sample_wpa_supplicant.conf
+
+if grep -Fq "$SSID" /etc/wpa_supplicant/wpa_supplicant.conf
+then
+    # code if found
+    echo "ssid already exists"
+    exit 1
+else
+    # code if not found
 cat >> /etc/wpa_supplicant/wpa_supplicant.conf << EOM
 
 network={
@@ -12,3 +23,19 @@ network={
 EOM
 
 systemctl restart networking.service
+
+fi
+
+
+#cat >> /etc/wpa_supplicant/wpa_supplicant.conf << EOM
+
+#network={
+#    ssid="$1"
+#    scan_ssid=1
+#    key_mgmt=WPA-PSK
+#    psk="$2"
+#    priority=$3
+#}
+#EOM
+
+#systemctl restart networking.service
